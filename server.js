0d5d802dff6a3aa1d9fde3fbffad55c7662909e1
speedyfriend433 <speedyfriend13@gmail.com>
2024-07-30 23:33:22 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
