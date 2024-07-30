5311ef12a1d27083eebb95515c01a16eee4e5e02
speedyfriend433 <speedyfriend13@gmail.com>
2024-07-30 22:25:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ wss.on('connection', (ws) => {
     ws.on('message', (message) => {
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
+                client.send(JSON.stringify({ type: 'message', text: message }));
             }
         });
     });
