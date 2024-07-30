0dcfa194878228d23cecea6790eba86965ccc343
speedyfriend433 <speedyfriend13@gmail.com>
2024-07-30 22:15:39 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const mongoose = require('mongoose');
-
-// MongoDB connection
-mongoose.connect('mongodb://localhost:27017/chat', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const connectionSchema = new mongoose.Schema({
-    totalConnections: { type: Number, default: 0 }
-});
-
-const Connection = mongoose.model('Connection', connectionSchema);
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 let currentConnections = 0;
+let totalConnections = 0;
 
 wss.on('connection', (ws) => {
     currentConnections++;
+    totalConnections++;
 
-    Connection.findOneAndUpdate({}, { $inc: { totalConnections: 1 } }, { new: true, upsert: true }, (err, doc) => {
-        if (err) console.error(err);
-        ws.send(JSON.stringify({ type: 'stats', currentConnections, totalConnections: doc.totalConnections }));
-    });
+    ws.send(JSON.stringify({ type: 'stats', currentConnections, totalConnections }));
 
     wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
